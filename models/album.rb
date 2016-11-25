ce7812a356fd6f60b58f2ce1f26772217145c03d
Elgrogz <gregorgilchrist@gmail.com>
2016-11-25 10:53:50 +0000

    artist/album inits added

--- models/album.rb
+++ models/album.rb
@@ -1,1 +1,15 @@
 require_relative('/db/sql_runner')
+
+class Album
+
+  attr_accessor :title, :genre, :quantity
+  attr_reader :id, :artist_id
+
+  def initialize(options)
+    @id = nil || options['id'].to_i
+    @title = options['title'] 
+    @genre = options['genre'] 
+    @quantity = options['quantity'].to_i 
+    @artist_id = options['artist_id'].to_i 
+  end
+end
--- models/artist.rb
+++ models/artist.rb
@@ -1,1 +1,12 @@
 require_relative('/db/sql_runner')
+
+class Artist
+
+  attr_accessor :name
+  attr_reader :id
+
+  def initialize(options)
+    @id = nil || options['id'].to_i
+    @name = options['name'] 
+  end
+end
