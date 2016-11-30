b75c502a60cc4a1b0bd4121d74de55a38a33a9fd
Elgrogz <gregorgilchrist@gmail.com>
2016-11-30 10:37:32 +0000

    jeff sayings added

---
+++ models/jeff.rb
@@ -0,0 +1,15 @@
+class Jeff
+
+  def self.all
+    return ["Hang ten!",
+            "Sooo pitted!",
+            "Dude!",
+            "Gnarly!",
+            "Surf's up!",
+            "Life's a beach!",
+            "Sooo stoked!",
+            "Fin, noggin', duuuuude!",
+            "Come surf in my turf!"]
+  end
+
+end
